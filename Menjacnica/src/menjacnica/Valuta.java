e312d5244aea39e34745bc70cdc529d5045861ec
PetarLakcevic <petar.lakcevic@gmail.com>
2017-03-26 00:55:54 +0100

    Generisane equals, toString i hashCode. Prve dve i izmenjene.

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -54,4 +54,45 @@ public class Valuta {
 	public void setProdajni(double prodajni) {
 		this.prodajni = prodajni;
 	}
+	@Override
+	public String toString() {
+		return "Sifra: " + getSifra() + "\n + Skraceno: " + getSkracenica() + "\n Kupovni kurs: " + getKupovni()
+				+ "\n Srednji kurs: " + getSrednji() + "\n Prodajni kurs: " + getProdajni();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + sifra;
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Valuta)) {
+			return false;
+		}
+		Valuta nov = (Valuta) obj;
+		if (getSifra() == nov.getSifra() && getSkracenica() == nov.getSkracenica()) {
+			return true;
+		}
+		return false;
+	}
+
 }
