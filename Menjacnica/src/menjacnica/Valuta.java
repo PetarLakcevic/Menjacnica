7ee3855f49dbe78d23a0868e273390e1b0d1990e
PetarLakcevic <petar.lakcevic@gmail.com>
2017-03-26 00:54:03 +0100

    Prvi commit. Dodati atributi i odgovarajuce set i get metode. Tag ce biti PK1.

---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,57 @@
+package menjacnica;
+
+public class Valuta {
+
+	private int sifra;
+	private String naziv;
+	private String skracenica;
+	private double kupovni, srednji, prodajni;
+
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getSkracenica() {
+		return skracenica;
+	}
+
+	public void setSkracenica(String skracenica) {
+		this.skracenica = skracenica;
+	}
+
+	public double getKupovni() {
+		return kupovni;
+	}
+
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+
+	public double getSrednji() {
+		return srednji;
+	}
+
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+
+	public double getProdajni() {
+		return prodajni;
+	}
+
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+}
