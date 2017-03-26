61c95f684b67a9487828d39a1bc304193aea21b9
PetarLakcevic <petar.lakcevic@gmail.com>
2017-03-26 01:09:41 +0100

    Mala ispravka i dodavanje komentara. Commit i PUSH.

--- Menjacnica/src/interfacemenjacnica/MenjacnicaInterface.java
+++ Menjacnica/src/interfacemenjacnica/MenjacnicaInterface.java
@@ -3,7 +3,7 @@ package interfacemenjacnica;
 import java.util.GregorianCalendar;
 
 public interface MenjacnicaInterface {
-	public void dodajKurs(int sifra, double[] kursevi, GregorianCalendar dan);
+	public void dodajKurs(int sifra, double[] kursevi, GregorianCalendar dan); //proslednjujemo sifru date valute, niz double-ova koji sadrzi prodajni, srednji i kupovni kurs kao i datum
 	public void obrisiKurs(int sifra, GregorianCalendar dan);
-	public double[] vratiKurs(int sifra, GregorianCalendar dan);
+	public double[] vratiKurs(int sifra, GregorianCalendar dan); //vraca sva 3 kursa u niz double-ova
 }
