9544d3ce4a660234b056ee2a0b83943c658c937f
PetarLakcevic <petar.lakcevic@gmail.com>
2017-03-26 01:12:58 +0100

    Dodata klasa Menjacnica koja implementuje MenjacnicaInterface

---
+++ Menjacnica/src/menjacnica/Menjacinca.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfacemenjacnica.MenjacnicaInterface;
+
+public class Menjacinca implements MenjacnicaInterface {
+
+	@Override
+	public void dodajKurs(int sifra, double[] kursevi, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(int sifra, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double[] vratiKurs(int sifra, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
